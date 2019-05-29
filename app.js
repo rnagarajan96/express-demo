6d8bf81f070bb2502874a37c487329081dc0af67
Nagarajan R <nagarajan@aagnia.com>
2019-05-29 23:03:47 +0530

    find method

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
-
+const courses = [
+    { id: 1, name: "node" },
+    { id: 2, name: "laravel" },
+    { id: 3, name: "angular" },
+]
 app.get('/', (req, res) => {
     res.send("Hello iam Nagarajan....");
 });
-app.get('/:event/:sport', (req, res) => {
-    res.send(`Event >>>>${req.params.event} >>>> Sport >>>>${req.params.sport}`);
+app.get('/courses', (req, res) => {
+    res.send(courses);
 });
-
+app.get('/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id == parseInt(req.params.id))
+    if (course) {
+        res.send(course);
+    } else {
+        res.send("fail");
+    }
+});
 const port = process.env.PORT || 3000;
 app.listen(port, () => { console.log(`app Listening on ${port}`) })
